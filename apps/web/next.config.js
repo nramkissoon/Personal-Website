93b420e9a120395540c2109c797d8a5e1751c708
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-12-15 23:47:55 -0500

    next config fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,23 +1,6 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require("next-transpile-modules")(["ui"], {resolveSymlinks: false});
 
 module.exports = withTM({
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx'],
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(ts)x??$/, // Just `tsx?` file only
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: "ts-loader",
-          options: {
-            transpileOnly: true,
-            onlyCompileBundledFiles: true,
-          },
-        },
-      ],
-    });
-  
-    return config;
-  },
 });

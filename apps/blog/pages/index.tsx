3c38b694a34c055f374434303a2a44a9220a6b9a
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-01-01 11:11:05 -0500

    get tailwind working

--- apps/blog/pages/index.tsx
+++ apps/blog/pages/index.tsx
@@ -1,20 +1,42 @@
 import fs from "fs";
 import matter from "gray-matter";
+import { InferGetServerSidePropsType } from "next";
+import Link from "next/link";
 import path from "path";
 
-export default function Docs() {
+const Post = (props: {
+  frontMatter: {
+    [key: string]: any;
+  };
+  slug: string;
+}) => {
+  return (
+    <div className="container mb-9">
+      <Link href={"/posts/" + props.slug} passHref>
+        {props.frontMatter.title}
+      </Link>
+    </div>
+  );
+};
+
+function Blog(props: InferGetServerSidePropsType<typeof getStaticProps>) {
   return (
     <div>
-      <h1>Docs</h1>
+      <h1 className="text-sm bg-slate-200">Blog</h1>
+      <div>
+        {props.posts.map((post) => (
+          <Post {...post} key={post.slug} />
+        ))}
+      </div>
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  const files = fs.readdirSync(path.join("posts"));
+  const files = fs.readdirSync("content/");
   const posts = files.map((filename) => {
     const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
+      path.join("content/", filename),
       "utf-8"
     );
     const { data: frontMatter } = matter(markdownWithMeta);
@@ -29,3 +51,5 @@ export const getStaticProps = async () => {
     },
   };
 };
+
+export default Blog;

1159fcb187e1cb62bd5b03414fa3e87ba8c8e5c9
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-10-31 18:24:33 -0400

    new og image

--- apps/blog/src/pages/api/og.tsx
+++ apps/blog/src/pages/api/og.tsx
@@ -6,10 +6,20 @@ export const config = {
   runtime: "experimental-edge",
 };
 
+const mediumInterFont = fetch(
+  new URL("../../../fonts/Inter-Medium.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+const boldInterFont = fetch(
+  new URL("../../../fonts/Inter-Bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
 export default async function handler(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const title = searchParams.get("title");
   const description = searchParams.get("description");
+
+  const fontData = [await mediumInterFont, await boldInterFont];
 
   if (!title || !description) {
     return new ImageResponse(
@@ -25,14 +35,54 @@ export default async function handler(req: NextRequest) {
             backgroundColor: "white",
           }}
         >
-          <div tw="flex flex-col w-full justify-center items-center">
-            <h2 tw="flex flex-col text-7xl font-extrabold tracking-tighter text-gray-900 text-center">
-              Developer Blog 👋
-            </h2>
-            <h3 tw="text-center tracking-tight text-gray-700 text-3xl ">
-              Blog covering software engineering topics, tutorials, personal
-              project updates, and more.
-            </h3>
+          <div tw="relative flex flex-col w-full justify-center items-center bg-transparent h-screen">
+            <div
+              tw="absolute inset-0"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to right, #8b5cf6, #f43f5e,#10b981 )",
+              }}
+            />
+            <div
+              tw="absolute inset-0 flex items-center justify-center overflow-hidden -left-[20px] -top-[20px]"
+              style={{
+                backgroundImage: `url(https://blog.nickramkissoon.com/bg.svg)`,
+              }}
+            />
+            <div
+              tw="absolute inset-0"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to right, rgba(17,17,17, 0.0), rgba(24,24,24, 0.7), rgba(17,17,17, 0.0))",
+              }}
+            />
+            <div
+              tw="absolute inset-0"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to bottom, rgba(24,24,24, 0), rgba(17,17,17, 0.7), rgba(24,24,24, 0))",
+              }}
+            />
+            <div tw="relative z-2 max-w-4xl p-7 flex-col items-center justify-center flex">
+              <h2
+                tw="text-7xl tracking-tighter text-zinc-50 w-full text-center"
+                style={{
+                  fontFamily: "Inter-bold",
+                }}
+              >
+                <span tw="text-6xl mr-4">💻</span> Developer Blog{" "}
+                <span tw="text-6xl ml-4">✍🏽</span>
+              </h2>
+              <h3
+                tw="text-center tracking-tight text-zinc-300 text-4xl "
+                style={{
+                  fontFamily: "Inter-med",
+                }}
+              >
+                Blog covering software engineering topics, tutorials, personal
+                project updates, and more.
+              </h3>
+            </div>
           </div>
         </div>
       ),
@@ -40,6 +90,18 @@ export default async function handler(req: NextRequest) {
         width: 1200,
         height: 630,
         emoji: "twemoji",
+        fonts: [
+          {
+            name: "Inter-med",
+            data: fontData[0],
+            style: "normal",
+          },
+          {
+            name: "Inter-bold",
+            data: fontData[1],
+            style: "normal",
+          },
+        ],
       }
     );
   }
@@ -57,19 +119,71 @@ export default async function handler(req: NextRequest) {
           backgroundColor: "white",
         }}
       >
-        <div tw="flex flex-col w-full justify-center items-center">
-          <h2 tw="flex flex-col text-7xl font-extrabold tracking-tighter text-gray-900 text-center">
-            {title} 👋
-          </h2>
-          <h3 tw="text-center tracking-tight text-gray-700 text-3xl ">
-            {description}
-          </h3>
+        <div tw="relative flex flex-col w-full justify-center items-center bg-transparent h-screen">
+          <div
+            tw="absolute inset-0"
+            style={{
+              backgroundImage:
+                "linear-gradient(to right, #8b5cf6, #f43f5e,#10b981 )",
+            }}
+          />
+          <div
+            tw="absolute inset-0 flex items-center justify-center overflow-hidden -left-[20px] -top-[20px]"
+            style={{
+              backgroundImage: `url(https://blog.nickramkissoon.com/bg.svg)`,
+            }}
+          />
+          <div
+            tw="absolute inset-0"
+            style={{
+              backgroundImage:
+                "linear-gradient(to right, rgba(17,17,17, 0.0), rgba(24,24,24, 0.7), rgba(17,17,17, 0.0))",
+            }}
+          />
+          <div
+            tw="absolute inset-0"
+            style={{
+              backgroundImage:
+                "linear-gradient(to bottom, rgba(24,24,24, 0), rgba(17,17,17, 0.7), rgba(24,24,24, 0))",
+            }}
+          />
+          <div tw="relative z-2 max-w-4xl p-7 flex-col items-center justify-center flex">
+            <h2
+              tw="text-7xl tracking-tighter text-zinc-50 w-full text-center"
+              style={{
+                fontFamily: "Inter-bold",
+              }}
+            >
+              {title} 💻
+            </h2>
+            <h3
+              tw="text-center tracking-tight text-zinc-300 text-4xl "
+              style={{
+                fontFamily: "Inter-med",
+              }}
+            >
+              {description}
+            </h3>
+          </div>
         </div>
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      emoji: "twemoji",
+      fonts: [
+        {
+          name: "Inter-med",
+          data: fontData[0],
+          style: "normal",
+        },
+        {
+          name: "Inter-bold",
+          data: fontData[1],
+          style: "normal",
+        },
+      ],
     }
   );
 }

f4d01bb083db1ef10ec3260381a83af83ae077f6
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-10-31 20:52:55 -0400

    eslint?

--- apps/blog/src/augment.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "react-syntax-highlighter" {
-  import { ReactNode } from "react";
-  export interface SyntaxHighlighterProps {
-    language: string;
-    style: any;
-  }
-  export class SyntaxHighlighter extends ReactNode<SyntaxHighlighterProps> {}
-}
